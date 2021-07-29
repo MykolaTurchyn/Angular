d28ec04078261467ce54c828481af5646ce8d6f0
MykolaTurchn14 <tyrchnmike@gmail.com>
2021-07-29 11:17:37 +0300

    main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {UsersComponent} from "./components/users/users.component";
-import {PostsComponent} from "./components/posts/posts.component";
-import {UsersDetailsComponent} from "./components/users-details/users-details.component";
-import {PostsDetailsComponent} from "./components/posts-details/posts-details.component";
 
-const routes: Routes = [
-  {
-    path: 'users', component: UsersComponent,
-    children: [
-      {path: ':id/posts', component: UsersDetailsComponent},
-    ]
-  },
-  // {path: 'users/:id', component: UsersDetailsComponent},
-  {
-    path: 'posts', component: PostsComponent,
-    children: [
-      {path: ':id/comments', component: PostsDetailsComponent}
-    ]
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsersComponent } from './components/users/users.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { UserComponent } from './components/user/user.component';
-import { PostComponent } from './components/post/post.component';
-import {HttpClientModule} from "@angular/common/http";
-import { UsersDetailsComponent } from './components/users-details/users-details.component';
-import { PostsDetailsComponent } from './components/posts-details/posts-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    PostsComponent,
-    UserComponent,
-    PostComponent,
-    UsersDetailsComponent,
-    PostsDetailsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
